c4e0011e776e1276a87418b1aff64f82912180b6
jankovviak <101602329+jankovviak@users.noreply.github.com>
2022-04-01 15:20:18 +0200

    Add files via upload

---
+++ stop.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+let text = '';
+
+for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  text = text + i;
+}
+
+console.log(text);
+// expected output: "012456789"
